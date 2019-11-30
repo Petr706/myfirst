d166708c930976023db2f46bc1a04cb8810df54a
Petr <petr.podolskiy@gmail.com>
2019-11-30 19:40:12 +0300

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$( document ).ready(function() {
+  $("#btn").click(
+  function(){
+    sendAjaxForm('result_form', 'ajax_form', 'action_ajax_form.php');
+    return false; 
+  }
+);
+});
+
+function sendAjaxForm(result_form, ajax_form, url {
+  $.ajax({
+      url:     action_ajax_form.php, //url страницы (action_ajax_form.php)
+      type:     "POST", //метод отправки
+      dataType: "html", //формат данных
+      data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
+      success: function(response) { //Данные отправлены успешно
+        result = $.parseJSON(response);
+        $('#result_form').html('Имя: '+result.name+'<br>Телефон: '+result.phonenumber);
+    },
+    error: function(response) { // Данные не отправлены
+          $('#result_form').html('Ошибка. Данные не отправлены.');
+    }
+ });
+}
+
+}$(document).ready(function() {  
+  $("#validate").keyup(function(){
+     
+     var email = $("#validate").val();
+   
+     if(email != 0)
+     {
+     if(isValidEmailAddress(email))
+     {
+     $("#validEmail").css({
+   "background-image": "url('validyes.png')"
+     });
+     } else {
+     $("#validEmail").css({
+   "background-image": "url('validno.png')"
+     });
+     }
+     } else {
+     $("#validEmail").css({
+   "background-image": "none"
+     }); 
+     }
+   
+     });
+   
+     });
+   
+     function isValidEmailAddress(emailAddress) {
+     var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+     return pattern.test(emailAddress);
+     }
