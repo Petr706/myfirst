f9d26c639aaf911846374cdaad605ac5854d00ed
Petr <petr.podolskiy@gmail.com>
2019-11-30 20:10:17 +0300

    Initial commit.

--- js/main.js
+++ js/main.js
@@ -9,15 +9,15 @@ $( document ).ready(function() {
 
 function sendAjaxForm(result_form, ajax_form, url {
   $.ajax({
-      url:     action_ajax_form.php, //url страницы (action_ajax_form.php)
-      type:     "POST", //метод отправки
-      dataType: "html", //формат данных
-      data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
-      success: function(response) { //Данные отправлены успешно
+      url:     action_ajax_form.php, 
+      type:     "POST", 
+      dataType: "html", 
+      data: $("#"+ajax_form).serialize(),  
+      success: function(response) { 
         result = $.parseJSON(response);
         $('#result_form').html('Имя: '+result.name+'<br>Телефон: '+result.phonenumber);
     },
-    error: function(response) { // Данные не отправлены
+    error: function(response) { 
           $('#result_form').html('Ошибка. Данные не отправлены.');
     }
  });
